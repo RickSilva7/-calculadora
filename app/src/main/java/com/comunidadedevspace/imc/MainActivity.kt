bb49804bfd39a6a68a735e36133c029bfd3104c0
Henrique <rick-silva8@outlook.com>
2024-11-24 13:13:45 +0100

    Tela Inicial EDTT

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -15,10 +15,12 @@ class MainActivity : AppCompatActivity() {
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
-        btnCalcular.setOnClickListener {
-            val peso = edtPeso.text
-            val Altura = edtAltura.text
-            println ("henrique" + Altura)
+        btnCalcular.setOnClickListener{
+            val peso: Float = edtPeso.text.toString().toFloat()
+            val altura: Float = edtAltura.text.toString().toFloat()
+            val alturaQ2 = altura * altura
+            val resultado = peso / alturaQ2
+            println ("Henrique" + resultado)
         }
     }
 }
